6ba8f838d3bf1940dfd79bf9a4d1e3b2f3ee4481
Eren <970988031@qq.com>
2022-03-08 16:52:34 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: Eren Lu
+ * @Date: 2022-03-08 14:21:20
+ * @LastEditors: Eren Lu
+ * @LastEditTime: 2022-03-08 15:11:31
+ * @FilePath: \Eren_testd:\Users\Administrator\Desktop\CICD\init\vite.config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by Eren Lu, All Rights Reserved. 
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    base: './',
+    plugins: [vue()]
 })
